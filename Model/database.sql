07584ffc17660f5fc907a9c794bc882adbc0c2f9
sebastianif <simitolafernandez@gmail.com>
2026-03-19 09:04:58 +0100

    Co-authored-by: JohnnyRib <JohnnyRib@users.noreply.github.com>

--- Model/database.sql
+++ Model/database.sql
@@ -3,11 +3,17 @@ Create database gameland;
 
 Use gameland;
 
+-- Tabla Usuarios --
 create table users (
     id int auto_increment primary key,
     username varchar(255) not null,
     email varchar(255) not null,
-    password varchar(255) not null
-    role ENUM('admin', 'user') not null default 'user'
-    image varchar(255) null
+    password varchar(255) not null,
+    role ENUM('admin', 'user') not null default 'user',
+    image longblob not null; 
 );
+
+
+insert into users (username, email, password, role, image )
+values(?,?,?,?,?);
+-- Tabla Eventos
