47dab726d74b4605c192f63902f096a7ade78010
sebastianif <simitolafernandez@gmail.com>
2026-03-25 08:54:47 +0100

    Cambio Controlador

--- Model/BD.php
+++ Model/BD.php
@@ -1,3 +1,25 @@
 <?php
 
+//datos de conexción
+
+$host= "localhost";
+$usuario= "root";
+$password="";
+$base_datos="Base_de_Datos";
+
+$conexion = new mysqli($host, $usuario, $password, $base_datos); 
+
+if($conexion-> connect_error) {
+    die("Error de Conexion: " . $conexion-> connect_error);
+
+}
+
+    echo "Conexión exitosa";
+
+    $conexion -> set_charset("utf8mb4");
+
+    $conexion -> close();
+
+?>
+
 
